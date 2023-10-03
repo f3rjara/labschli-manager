f882c2fbf7b3eb168b5e678b8a1064ad84ec42ad
Daniel Cabrera <ldanielcabreral@gmail.com>
2023-10-02 21:27:15 -0500

    feat(admin-component): :sparkles: se crean las vistas de administrador

---
+++ src/app/ui/roles/pages/roles/roles.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-roles',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './roles.component.html',
+  styleUrls: ['./roles.component.scss']
+})
+export class RolesComponent {
+
+}
